44692af533c56ce957214bd9ec95aa3ec3670fee
jeonghwan <jnghwn99@gmail.com>
2025-06-24 22:19:18 +0900

    feat(back, users-api): reconnect db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ export class AuthService {
   constructor(private readonly httpService: HttpService) {}
 
   kakaoLogin() {
-    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
+    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code&prompt=login`;
     return kakaoAuthUrl;
   }
 
@@ -41,10 +41,31 @@ export class AuthService {
         },
       );
       console.log(response.data);
-      return response.data;
+      const userInfo = await this.getKakaoUserInfo(response.data.access_token);
+      console.log(userInfo);
     } catch (error) {
       console.error(error);
       throw error;
+    }
+  }
+
+  async getKakaoUserInfo(accessToken: string) {
+    try {
+      const response = await this.httpService.axiosRef.get(
+        'https://kapi.kakao.com/v2/user/me',
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        '사용자 정보 조회 실패',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(process.cwd(), 'key.pem')),
-    cert: fs.readFileSync(path.join(process.cwd(), 'cert.pem')),
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
-  // const app = await NestFactory.create(AppModule);
+  // const httpsOptions = {
+  //   key: fs.readFileSync(path.join(process.cwd(), 'key.pem')),
+  //   cert: fs.readFileSync(path.join(process.cwd(), 'cert.pem')),
+  // };
+  // const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,10 +21,8 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: 'http://localhost:5173',
-
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 허용할 HTTP 메서드
-    credentials: true, // 인증 정보(쿠키, 인증 헤더 등)를 포함한 요청 허용 여부
-    // preflight requests에 대한 응답 상태 코드 (일부 레거시 브라우저 호환성을 위해 200으로 설정하기도 함)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
     optionsSuccessStatus: 200,
   });
   await app.listen(process.env.PORT || 3000);
