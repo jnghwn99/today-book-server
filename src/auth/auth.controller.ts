438bfc6e18366aa182fc85124af02c55b0859faf
jeonghwan <jnghwn99@gmail.com>
2025-07-11 14:11:35 +0900

    fix(auth): 로그아웃 메서드의 Response 옵션 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -29,7 +29,7 @@ export class AuthController {
 	}
 
 	@Post('logout')
-	logout(@Res() res: Response) {
+	logout(@Res({ passthrough: true }) res: Response) {
 		return this.authService.logout(res);
 	}
 }
